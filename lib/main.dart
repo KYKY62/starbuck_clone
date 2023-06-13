2da064e3ff204b159213d3a43bca8b7f574070bd
Your Name <you@example.com>
2023-06-13 19:55:55 +0700

    Set-Up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:starbuck_clone/routes/pages.dart';
 import 'package:starbuck_clone/routes/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
