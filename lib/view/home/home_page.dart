1ea2cabae1ec62f66cf3cfdedf40571126da57f9
Your Name <you@example.com>
2023-08-24 19:43:15 +0700

    appbar home

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -8,21 +8,83 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text("Home"),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              Get.back();
-              Get.back();
-            },
-            icon: const Icon(Icons.logout),
-          ),
-        ],
+      drawer: Drawer(
+        backgroundColor: Colors.amber,
+        child: ListView(
+          children: const [
+            DrawerHeader(
+              child: Icon(Icons.abc),
+            ),
+          ],
+        ),
       ),
-      body: const Center(child: Text("Ini Home")),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Builder(
+                    builder: (context) {
+                      return GestureDetector(
+                        onTap: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                        child: const Icon(
+                          Icons.menu,
+                          size: 24.0,
+                        ),
+                      );
+                    },
+                  ),
+                  const Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                          "https://i.ibb.co/PGv8ZzG/me.jpg",
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 10,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Good Afternoon,"),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text("Rizky Akbar Siregar"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      await FirebaseAuth.instance.signOut();
+                      Get.back();
+                      Get.back();
+                    },
+                    child: const Icon(
+                      Icons.logout,
+                      size: 24.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
